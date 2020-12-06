66a156702eefe74d78c953c2da8146c9ed78e123
erobakiewicz <erobakiewicz@gmail.com>
2020-12-06 19:57:36 +0100

    API REST settings

--- e_learning_Django3/settings.py
+++ e_learning_Django3/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'students.apps.StudentsConfig',
     'embed_video',
     'memcache_status',
+    'rest_framework',
 
 ]
 
@@ -149,5 +150,11 @@ CACHES = {
 }
 
 CACHE_MIDDLEWARE_ALIAS = 'default'
-CACHE_MIDDLEWARE_SECONDS = 60 * 15 # 15 minutes
+CACHE_MIDDLEWARE_SECONDS = 60 * 15  # 15 minutes
 CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
